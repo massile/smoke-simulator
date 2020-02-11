bbb57c768f64893066d738fdaabd4392eb660230
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2020-02-11 22:16:06 +0100

    Drastically speed up image writing

--- image/Ppm.h
+++ image/Ppm.h
@@ -8,13 +8,20 @@ namespace Image {
         Ppm(int width, int height) : AbstractImage(width, height) {}
 
         void Write(const char* filename) const override {
-            std::ofstream file(filename);
-            file << "P3" << std::endl << width << ' ' << height << std::endl << 255 << std::endl;
+            char* colorBuffer = new char[width*height*3];
+            int index = 0;
+            for (int y = height - 1; y >= 0; y--)
+            for (int x = 0; x < width; x++) {
+               const Color& pixel = pixels[x + y*width];
+               colorBuffer[index++] = 255.f * pixel.r;
+               colorBuffer[index++] = 255.f * pixel.g;
+               colorBuffer[index++] = 255.f * pixel.b;
+            }
 
-            for(int y = height - 1; y >= 0; y--) {
-            for(int x = 0; x < width; x++) {
-               file << Math::Vector<int>(255.f * pixels[x + y*width]);
-            }}
+            std::ofstream file(filename, std::ofstream::binary);
+            file << "P6\n" << width << " " << height << "\n" << "255\n";
+            file.write(colorBuffer, width*height*3);
+            file.close();
         }
     };
 }
