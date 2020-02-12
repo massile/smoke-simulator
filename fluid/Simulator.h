6a5904b74ad07996dc25f25613ece243c6ad0334
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2020-02-12 04:31:55 +0100

    Add advection for velocity and temperature

--- fluid/Simulator.h
+++ fluid/Simulator.h
@@ -3,7 +3,17 @@
 #include "./Renderer.h"
 
 namespace Fluid {
-    __global__ void Advection(float* prevAtt, float* attenuation, Math::Direction* velocity, float dt, float dissipation);
+    template<typename T>
+    __global__
+    void Advection(T* prevValue, T* currentValue, Math::Direction* velocity, float dt, float dissipation) {
+        int x = threadIdx.x + blockDim.x * blockIdx.x;
+        int y = threadIdx.y + blockDim.y * blockIdx.y;
+        int z = threadIdx.z + blockDim.z * blockIdx.z;
+
+        const Math::Point voxel(x, y, z);
+        const Math::Direction v = velocity[VoxelIndex(voxel)];
+        currentValue[VoxelIndex(voxel)] = dissipation * prevValue[VoxelIndex(voxel - v*dt)];
+    }
 
     void Simulate(Renderer& renderer, Image::AbstractImage& image) {
         SmokeBall& gas = renderer.gas;
@@ -14,24 +24,21 @@ namespace Fluid {
         for (int frame = 0; frame < 250; frame++) {
             renderer.RenderImage(image);
 
+            Advection<<<numThreads, numBlocks>>>(gas.velocity.previous, gas.velocity.current, gas.velocity.current, dt, 1.f);
+            cudaDeviceSynchronize();
+            gas.velocity.Swap();
+
             Advection<<<numThreads, numBlocks>>>(gas.attenuation.previous, gas.attenuation.current, gas.velocity.current, dt, 0.98f);
             cudaDeviceSynchronize();
             gas.attenuation.Swap();
+
+            Advection<<<numThreads, numBlocks>>>(gas.temperature.previous, gas.temperature.current, gas.velocity.current, dt, 0.999f);
+            cudaDeviceSynchronize();
+            gas.temperature.Swap();
 
             std::stringstream fileName;
             fileName << "out/" << frame << ".ppm";
             image.Write(fileName.str().c_str());
         }
-    }
-
-    __global__
-    void Advection(float* prevAtt, float* attenuation, Math::Direction* velocity, float dt, float dissipation) {
-        int x = threadIdx.x + blockDim.x * blockIdx.x;
-        int y = threadIdx.y + blockDim.y * blockIdx.y;
-        int z = threadIdx.z + blockDim.z * blockIdx.z;
-
-        const Math::Point voxel(x, y, z);
-        const Math::Direction v = velocity[VoxelIndex(voxel)];
-        attenuation[VoxelIndex(voxel)] = dissipation * prevAtt[VoxelIndex(voxel - v*dt)];
     }
 }
